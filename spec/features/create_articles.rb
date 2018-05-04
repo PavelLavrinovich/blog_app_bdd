181894ecadb7cd7d8c0e0906f14ecd5224f83981
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-05-04 17:33:59 +0300

    Create article green

---
+++ spec/features/create_articles.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature 'Create articles' do
+  scenario 'A user creates a new article' do
+    visit '/'
+
+    click_link 'New article'
+
+    fill_in 'Title', with: 'Test title'
+    fill_in 'Body', with: 'Test body'
+    click_button 'Create Article'
+
+    expect(page).to have_content('Article has been created')
+    expect(page.current_path).to eq(articles_path)
+  end
+end
