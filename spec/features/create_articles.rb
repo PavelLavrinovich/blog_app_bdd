bae54a2efdd086178355d096d2d3e627c241fb98
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-05-08 11:38:30 +0300

    Empty title and body validation

--- spec/features/create_articles.rb
+++ spec/features/create_articles.rb
@@ -13,4 +13,18 @@ RSpec.feature 'Create articles' do
     expect(page).to have_content('Article has been created')
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario 'A user creates an article with an empty title and body' do
+    visit '/'
+
+    click_link 'New article'
+
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: ''
+    click_button 'Create Article'
+
+    expect(page).to have_content('Article has not been created')
+    expect(page).to have_content('Title can\'t be blank')
+    expect(page).to have_content('Body can\'t be blank')
+  end
 end
